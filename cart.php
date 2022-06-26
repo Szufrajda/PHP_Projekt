29b4bcbe2190812cff039090ec8b1dc7b643a682
Szufrajda <91895457+Szufrajda@users.noreply.github.com>
2022-06-26 23:07:43 +0200

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+ob_start();
+// include header.php file
+include ('header.php');
+?>
+
+<?php
+
+/*  include cart items if it is not empty */
+count($product->getData('cart')) ? include ('Template/_cart-template.php') :  include ('Template/notFound/_cart_notFound.php');
+/*  include cart items if it is not empty */
+
+///*  include wishlist section */
+//count($product->getData('wishlist')) ? include ('Template/_wishlist_template.php') :  include ('Template/notFound/_wishlist_notFound.php');
+///*  include wishlist section */
+
+
+/*  include promotion section */
+include ('Template/_promotion.php');
+/*  include promotion section */
+
+?>
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
---
+++ product.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+// include header.php file
+include ('header.php');
+?>
+
+<?php
+
+/*  include products */
+include ('Template/_product.php');
+/*  include products */
+
+/*  include top sale section */
+include ('Template/_top-sale.php');
+/*  include top sale section */
+
+?>
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
